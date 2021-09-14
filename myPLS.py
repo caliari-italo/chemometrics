b86ea77e67d44d1ed2a47f1d6301f275ca0db15b
Ítalo Caliari <43014124+caliariitalo@users.noreply.github.com>
2021-09-14 11:07:04 -0300

    Add files via upload

--- myPLS.py
+++ myPLS.py
@@ -48,7 +48,7 @@ def runPLS(X_train, Y_train, X_test, Y_test, n_components, cv=10, plot='off'):
     output = pd.DataFrame([n_components, RMSEC, R2, RMSECV, R2CV, RMSEP, R2P], 
                           index=['Components', 'RMSEC', 'R2', 'RMSECV', 'R2CV', 'RMSEP', 'R2P']
                           ).T#.set_index('Components')
-    
+
     return output
 
 def optPLS(X_train, Y_train, X_test, Y_test, max_components, cv=10, plot='off'):
@@ -59,10 +59,10 @@ def optPLS(X_train, Y_train, X_test, Y_test, max_components, cv=10, plot='off'):
     import matplotlib.pyplot as plt
 
     output = pd.DataFrame()
-    
+
     for n_components in range(1, max_components+1):
         output = output.append(runPLS(X_train, Y_train, X_test, Y_test, n_components, cv, plot='off'))
-    
+
     if plot == 'on':
         fig = plt.figure(figsize = (5,5), dpi=300)
         RMSEplot = fig.add_subplot(1,1,1) 
@@ -78,7 +78,7 @@ def optPLS(X_train, Y_train, X_test, Y_test, max_components, cv=10, plot='off'):
         RMSEplot.set_xlabel('LV', fontsize = 10)
         RMSEplot.set_ylabel('A. U.', fontsize = 10)
         RMSEplot.legend()
-        
+
         fig = plt.figure(figsize = (5,5), dpi=300)
         R2plot = fig.add_subplot(1,1,1) 
         R2plot.plot(np.arange(1, max_components+1),
@@ -93,23 +93,23 @@ def optPLS(X_train, Y_train, X_test, Y_test, max_components, cv=10, plot='off'):
         R2plot.set_xlabel('LV', fontsize = 10)
         R2plot.set_ylabel('A. U.', fontsize = 10)
         R2plot.legend()
-        
+
     return output
 
 def autoPLS(X_train, Y_train, X_test, Y_test, max_components, cv=10, plot='off'):
     "bestoutput = autoPLS(X_train, Y_train, X_test, Y_test, max_components, cv=10, plot='off')"
 
     import numpy as np
-    
+
     output = optPLS(X_train, Y_train, X_test, Y_test, max_components, cv, plot)
-    
+
     diff = np.diff(output.RMSECV)
-    
+
     for n_components in range(1,len(diff)+1):
         if diff[n_components-1]/output.RMSECV.iloc[n_components-1] > -0.1: break
-    
+
     bestoutput = runPLS(X_train, Y_train, X_test, Y_test, n_components, cv, plot)
-    
+
     return bestoutput
 
 def sgPLS(X_train, Y_train, X_test, Y_test, max_components, window_length, polyorder, deriv, cv=10, plot='off'):
@@ -126,12 +126,12 @@ def sgautoPLS(X_train, Y_train, X_test, Y_test, max_components, cv=10):
     "bestoutput = sgPLS(X_train, Y_train, X_test, Y_test, max_components, cv=10)"
     import pandas as pd
     #from scipy.signal import savgol_filter
-    
+
     sg = pd.DataFrame()
     output = pd.DataFrame()
     bestoutput = pd.DataFrame()
     run = 0
-    
+
     for window_length in range(5, int(len(X_test.T)*.15), 2):
         for deriv in range(0, 3):
             for polyorder in range(deriv, 3):
@@ -139,14 +139,10 @@ def sgautoPLS(X_train, Y_train, X_test, Y_test, max_components, cv=10):
                 sg = sg.append((pd.DataFrame([run, window_length, polyorder, deriv],
                                              index=['Run','Window', 'PolyOrder', 'Deriv']))
                                .T)
-                #X_train_sg = savgol_filter(X_train, window_length, polyorder, deriv)
-                #X_test_sg = savgol_filter(X_test, window_length, polyorder, deriv)
-                #output = autoPLS(X_train_sg, Y_train, X_test_sg, Y_test, 15, cv=10, plot='off')
-                
                 output = sgPLS(X_train, Y_train, X_test, Y_test, max_components, window_length, polyorder, deriv, cv, plot='off')
                 output['Run'] = run
                 bestoutput = bestoutput.append(output)
-    
+
     sg = sg.set_index('Run')
     bestoutput = bestoutput.set_index('Run')
     bestoutput = bestoutput.join(sg)
--- temp.py
+++ temp.py
@@ -5,14 +5,26 @@ Created on Mon Sep 13 12:32:37 2021
 
 @author: caliariitalo
 """
-
-from myPLS import autoPLS
+import pandas as pd
+import numpy as np
+from myPLS import sgautoPLS
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_boston
+from scipy.signal import savgol_filter
+import matplotlib.pyplot as plt
 
 
-Xraw, Yraw = load_boston(return_X_y=True)
+data = pd.read_csv('peach_spectra_brix.csv')
+
+#Xraw, Yraw = load_boston(return_X_y=True)
+# Get reference values
+Yraw = data['Brix'].values
+# Get spectra
+Xraw = data.drop(['Brix'], axis=1).values
+# Get wavelengths
+wl = np.arange(1100,2300,2)
 
 X_train, X_test, Y_train, Y_test = train_test_split(Xraw, Yraw)
 
-bestoutput = autoPLS(X_train, Y_train, X_test, Y_test, 15, cv=10, plot='on')
+bestoutput = sgautoPLS(X_train, Y_train, X_test, Y_test, 5, cv=10)
+
